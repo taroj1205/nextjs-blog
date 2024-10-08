9d35fd269dd11d66ee904abc81e042cef1f74ee7
taroj1205 <taroj1205@gmail.com>
2024-10-08 13:28:40 +1300

    refactor(page): removed generate static params

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -6,12 +6,6 @@ import { Badge } from "@/components/ui/badge";
 import { MDXContent } from "@content-collections/mdx/react";
 import { Toc } from "@/components/toc";
 
-export const generateStaticParams = async () => {
-	const posts = allPosts.map((post) => post._meta.path);
-
-	return posts;
-};
-
 export const generateMetadata = ({ params }: { params: { slug: string } }) => {
 	const post = allPosts.find((post) => post._meta.path === params.slug);
 
